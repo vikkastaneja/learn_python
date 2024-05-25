07f3f8d4a082538cd31bedd6b622e1fc76990ebc
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-05-24 21:00:28 -0700

    Example of sharing data - Array and Variable, between processes

--- Multiprocessing.py
+++ Multiprocessing.py
@@ -1,31 +1,25 @@
 import time
 import multiprocessing
 import multiprocessing.process
-# We will create two processes doing two different operation on the same set of data
-result = []
-def calculate_square(arr):
-    for n in arr:
-        result.append(n*n)
 
-    print('square result: ' + str(result))
+def calculate_square(arr, result, sum1):
+    for idx, n in enumerate(arr):
+        result[idx] = n*n
 
-
-def calculate_cube(arr):
-    for n in arr:
-        result.append(n*n*n)
-
-    print('cube result: ' + str(result))
+    sum1.value = sum(result)
+    print('square result: ' + str(result[:]))
+    print('square sum: ' + str(sum1.value))
 
 if __name__ == "__main__":
     arr = [1,2,3,4,5,6,7,7,8,9]
-    p1 = multiprocessing.Process(target=calculate_square, args=(arr,))
-    p2 = multiprocessing.Process(target=calculate_cube, args=(arr,))
+
+    result = multiprocessing.Array('i', len(arr))
+    sum = multiprocessing.Value('i')
+    p1 = multiprocessing.Process(target=calculate_square, args=(arr, result, sum))
 
     p1.start()
-    p2.start()
-
     p1.join()
-    p2.join()
 
-    print('Global result: ' + str(result))
+    print('Global result: ' + str(result[:]))
+    print('Global sum: ' + str(sum.value))
     print('Done!')
