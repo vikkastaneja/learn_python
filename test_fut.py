03ca3988a150380bb83469c676465aafe9ef7e49
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-05-27 13:35:47 -0700

    paraterization of test values

--- test_fut.py
+++ test_fut.py
@@ -1,4 +1,13 @@
 import modules.fut as fut
+import pytest
 
-def test_calc_total():
-    assert fut.calc_total(1,2) == 3
+@pytest.mark.parametrize('test_input1, test_input2, expected_result',
+                            [
+                                (1,2,3),
+                                (2,3,5),
+                                (100, 400, 500),
+                                (-1, -2, -3)
+                            ])
+def test_calc_total(test_input1, test_input2, expected_result):
+    assert fut.calc_total(test_input1,test_input2) == expected_result
+
